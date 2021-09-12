279998f6e6b45e4155ad5584450a24762b4a674c
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-12 14:41:17 -0500

    routes are working again with new file structure

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db/db.json')
+
+router.get('/notes', (req, res) => {
+    res.json(db);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4()
+    db.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    res.json(req.body)
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let deleteBtnId = req.params.id;
+    const finalDb  = db.filter(note => note.id !== deleteBtnId);
+    fs.writeFile(JSON.stringify(db), JSON.stringify(finalDb), (err) => {
+        if (err) throw err;
+        console.log('The delete file has been saved!');
+    })
+    res.json(req.body);  
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,41 +1,17 @@
-const fs = require('fs');
 const express = require('express');
 const app = express();
-const path = require('path');
 const PORT = 3001;
-const db = require('./db/db.json')
-const { v4: uuidv4 } = require('uuid');
 
-app.use(express.urlencoded({ extended: true }));
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.json());
-
-
-app.use(express.static('./public'))
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-     res.json(db);
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-//POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-app.post('/api/notes', (req, res) => {
-    req.body.id = uuidv4()
-    db.push(req.body);
-    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      });
-      res.json(req.body)
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
-    console.log(`App listenig on PORT ${PORT}`);
+    console.log(`App listening on PORT ${PORT}`);
 });

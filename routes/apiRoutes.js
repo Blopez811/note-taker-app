af1916b66d4d0643fd87a9e51b62c740ee1cec29
Benicio Lopez <benicio.lopez@gmail.com>
2021-09-12 15:33:24 -0500

    delete endpoint is running now

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const db = require('../db/db.json')
+let db = require('../db/db.json')
 
 router.get('/notes', (req, res) => {
     res.json(db);
@@ -19,12 +19,15 @@ router.post('/notes', (req, res) => {
 
 router.delete('/notes/:id', (req, res) => {
     let deleteBtnId = req.params.id;
-    const finalDb  = db.filter(note => note.id !== deleteBtnId);
-    fs.writeFile(JSON.stringify(db), JSON.stringify(finalDb), (err) => {
+    db  = db.filter(note => note.id !== deleteBtnId);
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
         if (err) throw err;
         console.log('The delete file has been saved!');
     })
-    res.json(req.body);  
+    
+    res.sendStatus(204)
+    // res.json(req.body);  
 })
+
 
 module.exports = router
